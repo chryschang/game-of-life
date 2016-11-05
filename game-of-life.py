ccf1551b1449874b5708552006ba627ff384cbfd
Apgt512 <apgt@outlook.com>
2016-11-05 12:39:14 -0600

    Add files via upload

---
+++ game-of-life.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 12:05:48 2016
+
+@author: apgt
+"""
+import numpy as np
+
+#Creating the grid
+grid = np.zeros(10000).reshape(100,100)
+
+#Initial Pattern
+
+
+
+#Game
+
+
+
+
+
+#Print grid
